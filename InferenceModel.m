f7273cf96578797bae20751dc189a11e2806702b
Vishesh Goyal <vsg2608@gmail.com>
2018-10-09 20:49:58 +0530

    InferenceModel

--- InferenceModel.m
+++ InferenceModel.m
@@ -1,4 +1,3 @@
-A=importdata('Data@10.txt');    %Training Data
 
 I_0= 0.00258;              %Initial moles of initiator
 M_0= 1E6;                  %Initial moles of monomer
@@ -6,3 +5,6 @@ T=323.45;
 R_lm =0;
 Tf=10;
 C=MMA_Simulation(I_0, M_0, T, R_lm, Tf);
+
+Data= importdata('Data@10.txt');    %Training Data
+
