977266b68bd71220b041a0d4ef4451eb9369b76b
Vishesh Goyal <vsg2608@gmail.com>
2018-10-30 02:02:19 +0530

    Python model

--- PLS_Model.py
+++ PLS_Model.py
@@ -5,6 +5,8 @@ Created on Tue Oct 30 01:04:07 2018
 @author: Vishesh
 """
 from sklearn.cross_decomposition import PLSRegression
+from sklearn.metrics.pairwise import euclidean_distances
+import numpy as np
 
 #Data import
 #%%
@@ -15,8 +17,15 @@ Y=[]
 for line in Lines:
     line=line.split("\n")[0]
     Data=line.split("\t")
-    X.append(Data[:2])
-    Y.append(Data[2])
+    X.append([int(Data[0]),int(Data[1])])
+    Y.append(int(Data[2]))
+    
+Xpred=X[0]
+#Data filteration according to Euclidean distance
+#%%
+Xtrain=[]
+Ytrain=[]
+Xdist=euclidean_distances(X,[Xpred])
     
 #Partial Least Square Regression
 #%%
