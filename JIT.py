f8daca65cd74ae35091126a6c44073a4d8eee520
Vishesh Goyal <vsg2608@gmail.com>
2018-11-02 16:21:26 +0530

    Python model

--- JIT.py
+++ JIT.py
@@ -2,7 +2,9 @@ import matlab.engine
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics.pairwise import euclidean_distances
 import matplotlib.pyplot as plt
+import numpy as np
 from random import gauss
+import time
 eng = matlab.engine.start_matlab()
 eng.addpath(r'C:\Users\Vishesh\Desktop\Workspace\BTP',nargout=0)
 #%%
@@ -40,38 +42,64 @@ def writeData(s,X,Y,Xnorm):
     f=open(s,'a')
     f.write(str(X[0]*Xnorm[0])+"\t"+str(X[1]*Xnorm[1])+"\t"+str(Y)+"\n")
     f.close()
+
+def manhattan_distance(x,y):
+    return sum(abs(a-b) for a,b in zip(x,y))
   
-def euclideanPoints(Xdist,X,Y):
+def euclideanPoints(X,Y,Xpred):
+    Xdist=euclidean_distances(X,[Xpred])
     Xtrain=[]
     Ytrain=[]
-    for i in range(len(X)):
-        if(Xdist[i]<euclideanThreshold):
-            Xtrain.append(X[i])
-            Ytrain.append(Y[i])
-    return [Xtrain,Ytrain]
+    for k in range(10):
+        for i in range(len(X)):
+            if(Xdist[i]<euclideanThreshold*(2**k)):
+                Xtrain.append(X[i])
+                Ytrain.append(Y[i])
+        if(len(Xtrain)>minDataPoints):
+            return [Xtrain,Ytrain]
+        
+def manhattanPoints(X,Y,Xpred):
+    Xtrain=[]
+    Ytrain=[]
+    for k in range(10):
+        for i in range(len(X)):
+            if(manhattan_distance(X[i],Xpred)<euclideanThreshold*(2**k)):
+                Xtrain.append(X[i])
+                Ytrain.append(Y[i])
+        if(len(Xtrain)>minDataPoints):
+            return [Xtrain,Ytrain]
     
 #Just in Time function with partial linear regression as local model
 def predictConversion(Xpred):
     [X,Y]=readData("Data@10.txt")
     [Xpred]=normalize([Xpred],Xnorm)
     X=normalize(X,Xnorm)
-    Xdist=euclidean_distances(X,[Xpred])
-    [Xtrain,Ytrain]=euclideanPoints(Xdist,X,Y)
+    
+    [Xtrain,Ytrain]=euclideanPoints(X,Y,Xpred)
     pls2 = PLSRegression(n_components=2)
     pls2.fit(Xtrain, Ytrain)
     Ypredict = pls2.predict([Xpred],copy=True)
     Ypred=Ypredict[0][0]
     return Ypred
+
+def rmse(predictions, targets):
+    Sum=0
+    for i in range(len(predictions)):
+        Sum+=((predictions[i]-targets[i])**2)
+    return Sum/len(predictions)
+
     
 
-#%%
+#%% 
 n=100
 Tempratures=[gauss(323.15,10)for i in range(n)]
 R_lms= [gauss(1000,300)for i in range(n)]
 #%%
 Yactuals=[]
 Ypredicts=[]
+Time=[]
 for i in range(10):
+    currentTime=time.time()
     Xpred=[Tempratures[i],R_lms[i]]
     Ypred=predictConversion(Xpred)  #Prediction using JIT model
     Ypredicts.append(Ypred)
@@ -80,10 +108,15 @@ for i in range(10):
     Yactual=eng.MMA_Simulation(I_0, M_0, T, R_lm, Tf)   #Actual value from ODE
     writeData("Data@10.txt",Xpred,Yactual,Xnorm)        #writes data back to txt file
     Yactuals.append(Yactual)
+    Time.append(time.time()-currentTime)
+
+RMSE= rmse(Ypredicts,Yactuals)
+Correlation= np.corrcoef(Ypredicts,Yactuals)[1][0]
+averageTime=np.mean(Time)
 
 #%% PLots
 plt.style.use("default")
-plt.suptitle("Ypred vs Yact")
+plt.suptitle("Ypred vs Yact | Euclidean Distance\n RMSE= "+str(RMSE)+"\nCorr= "+str(Correlation)+"\nAverageTime= "+str(averageTime))
 plt.plot([-0.2,1,0,0,0,0,1,-0.2],[-0.2,1,0,1,-0.2,0,0,0],color='k',linewidth=1)
 plt.scatter(Yactuals,Ypredicts, color="red",marker="o",linewidth=0.1,alpha=0.7)
 plt.xlabel("Yactual")
